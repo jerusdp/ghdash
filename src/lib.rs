d035f63f468721bab3bcc841b4207c341b01c183
Jeremiah Russell <jerry@jrussell.ie>
2022-11-20 12:46:05 +0000

    feat: support libraty
    data stuctures and functions for github access

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![warn(missing_docs)]
+#![cfg_attr(docsrs, feature(rustdoc_missing_doc_code_examples))]
+#![cfg_attr(docsrs, warn(rustdoc::missing_doc_code_examples))]
+#![cfg_attr(docsrs, warn(rustdoc::invalid_codeblock_attributes))]
+
+//! Github Dashboard
+//!
+//! Provides a library of functions to build a dashboard for a github user.
+//!
+//! ## Usage
+//!
+//! Add the dependency to Cargo.toml
+//!
+//! ```toml
+//!
+//! [dependencies]
+//! ghdash = "0.1.0"
+//!
+//! ```
+//!
+//! ```no_run
+//! # type Error =
+//! # fn main() -> Result<(), Error> {
+//!
+//! # Ok(())
+//! #}
+//! ```
+//!
