ca4fbb4ed1366597e4e69fa9148d77c3ee2335f9
Jeremiah Russell <jerry@jrussell.ie>
2022-11-20 12:47:16 +0000

    feat(dashboard): Initial struct for basic dashboard
    user and token for access and repositories list of repositories

---
+++ src/dashboard.rs
@@ -0,0 +1,58 @@
+//! Dashboard module represents that data presented in the dashboard
+//!
+
+/// Struct Representing a Dashboard and key data required to create the dashboard
+///
+/// ## Fields
+/// - user - the github user for which the dashboard is created
+/// - token - a personal access token for the user that provides access to the github API
+/// - repositories - a list of the user's repositories presented in the dashboard
+///
+///
+#[derive(Debug)]
+pub struct Dashboard {
+    user: String,
+    token: String,
+    repositories: Vec<String>,
+}
+
+impl Dashboard {
+    /// Create a dashboard by setting the user and token strings to access Github
+    /// Without a user and token to get data from Github the dashboard is meaningless
+    /// therefore a new struct without this data is not meaningful
+    ///
+    pub fn new(user: &str, token: &str) -> Dashboard {
+        Dashboard {
+            user: user.to_string(),
+            token: token.to_string(),
+            repositories: vec![],
+        }
+    }
+
+    /// Get the user
+    ///
+    pub fn user(&self) -> &str {
+        &self.user
+    }
+
+    /// Set the user name in the Dashboard struct
+    ///
+    pub fn set_user(&mut self, user: &str) -> &mut Self {
+        self.user = user.to_string();
+        self
+    }
+
+    /// Set the token in the Dashboard
+    ///
+    pub fn set_token(&mut self, token: &str) -> &mut Self {
+        self.token = token.to_string();
+        self
+    }
+
+    /// Add a repo to the Dashboard
+    ///
+    pub fn add_repo(&mut self, repo: &str) -> &mut Self {
+        self.repositories.push(repo.to_string());
+        self
+    }
+}
