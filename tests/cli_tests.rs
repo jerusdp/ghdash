e6a0c39dd26351e9d7ccfe760e732a9dedeacd5b
Jeremiah Russell <jerry@jrussell.ie>
2024-02-25 08:41:11 +0000

    test: Duplicated cmd tests for with and without zipkin

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -4,3 +4,17 @@ fn cli_tests() {
         // .case("README.md")
         .case("tests/cmd/*.trycmd");
 }
+
+#[test]
+fn with_zipkin_tests() {
+    trycmd::TestCases::new()
+        // .case("README.md")
+        .case("tests/with_zipkin/*.trycmd");
+}
+
+#[test]
+fn without_zipkin_tests() {
+    trycmd::TestCases::new()
+        // .case("README.md")
+        .case("tests/without_zipkin/*.trycmd");
+}
