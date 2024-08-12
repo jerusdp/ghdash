9f43e8314083f867e2f6dbcbaa90243cc808443d
Jeremiah Russell <jerry@jrussell.ie>
2024-08-12 11:57:12 +0100

    refactor: move config from bin to library so that it can be shared with other bins
    * refactor(cli): move config.rs from cli to src root
    * feat(config.rs): add comments to GhConfig methods
    * feat(lib.rs): expose GhConfig in the root module
    * fix(main.rs): adjust import paths after moving config.rs

--- src/config.rs
+++ src/config.rs
@@ -12,6 +12,7 @@ use std::ffi::OsString;
 
 use serde_derive::{Deserialize, Serialize};
 
+/// Configuration data for the application
 #[derive(Default, Debug, Serialize, Deserialize, Clone)]
 pub struct GhConfig {
     user: String,
@@ -19,15 +20,18 @@ pub struct GhConfig {
 }
 
 impl GhConfig {
+    /// Create a new GhConfig
     pub fn user(&self) -> String {
         self.user.clone()
     }
 
+    /// Set the user github user name
     pub fn set_user(&mut self, user: &str) -> &mut Self {
         self.user = user.to_string();
         self
     }
 
+    /// Try to read the user from the environment
     pub fn try_env_user(&mut self, prefix: &str) -> &mut Self {
         let key = OsString::from(format!("{prefix}_USER").to_uppercase());
 
@@ -38,15 +42,18 @@ impl GhConfig {
         self
     }
 
+    /// Get the personal access token
     pub fn token(&self) -> String {
         self.token.clone()
     }
 
+    /// Set the personal access token
     pub fn set_token(&mut self, token: &str) -> &mut Self {
         self.token = token.to_string();
         self
     }
 
+    /// Try to read the token from the environment
     pub fn try_env_token(&mut self, prefix: &str) -> &mut Self {
         let key = OsString::from(format!("{prefix}_TOKEN").to_uppercase());
 
